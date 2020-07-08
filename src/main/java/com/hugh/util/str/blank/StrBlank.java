fb5952ec1f2a4c277fa4fb1cf32e18dc627757a2
FlyMeToTheMars <flyhobo@live.com>
2020-07-09 01:38:38 +0800

    commons v1.1

---
+++ src/main/java/com/hugh/util/str/blank/StrBlank.java
@@ -0,0 +1,21 @@
+package com.hugh.util.str.blank;
+
+import org.apache.commons.lang3.ArrayUtils;
+import org.w3c.dom.CharacterData;
+
+/**
+ * @Author Fly.Hugh
+ * @Date 2020/7/7 5:33 下午
+ * @Version 1.0
+ **/
+public class StrBlank {
+
+    /**
+     * CharSequence is a readable sequence of char values.
+     * This interface provides uniform, readonly access to many different kinds of char sequence.
+     * It is therefore inappropriate to use arbitrary CharSequence instance as elements in a set or as keys in a map.
+     * */
+    public static void main(String[] args) {
+        System.out.println((int)'a');
+    }
+}
